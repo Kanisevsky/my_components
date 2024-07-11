599b8b0eb68425a33d8bc109882c077a915d9a0c
Kanisevsky <meltsmokestudio@gmail.com>
2024-07-11 19:02:53 +0100

    header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -4,54 +4,84 @@ import './header.css';
 
 const Header = () => {
   return (
-    <header className="header">
-      <Image
-        src="./Ctech Europe.svg"
-        alt="ctech logo"
-        height={50}
-        width={150}
-      />
-      <nav>
-        <ul className="nav">
-          <li>
-            <Link href="systems" className="nav-text">
-              Systems
-            </Link>
-          </li>
-          <li>
-            <Link href="systems" className="nav-text">
-              PolyClay®️
-            </Link>
-          </li>
-          <li>
-            <Link href="systems" className="nav-text">
-              NewsRoom
-            </Link>
-          </li>
-          <li>
-            <Link href="systems" className="nav-text">
-              Case Studies
-            </Link>
-          </li>
-          <li>
-            <Link href="systems" className="nav-text">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="systems" className="nav-text">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="header-container-flags">
-        <Image src="./English Site.svg" alt="" width={24} height={24} />
-        <Image src="./German Site.svg" alt="" width={24} height={24} />
-        <Image src="./Spanish Site.svg" alt="" width={24} height={24} />
-      </div>
-      <div className="header-cta">
-        <a href="">Request a callback</a>
+    <header>
+      <div className="header-container">
+        <Link href="/">
+          <Image
+            src="./Ctech Europe.svg"
+            alt="ctech logo"
+            height={50}
+            width={130}
+            className="header-logo"
+          />
+        </Link>
+
+        <nav>
+          <ul className="nav">
+            <li>
+              <Link href="systems" className="nav-text">
+                Systems
+              </Link>
+            </li>
+            <li>
+              <Link href="systems" className="nav-text">
+                PolyClay®️
+              </Link>
+            </li>
+            <li>
+              <Link href="systems" className="nav-text">
+                NewsRoom
+              </Link>
+            </li>
+            <li>
+              <Link href="systems" className="nav-text">
+                Case Studies
+              </Link>
+            </li>
+            <li>
+              <Link href="systems" className="nav-text">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="systems" className="nav-text">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="header-flags">
+          <Link href="/eng">
+            <Image
+              src="./English Site.svg"
+              alt=""
+              width={22}
+              height={22}
+              className="flag"
+            />
+          </Link>
+          <Link href="/ger">
+            <Image
+              src="./German Site.svg"
+              alt=""
+              width={22}
+              height={22}
+              className="flag"
+            />
+          </Link>
+          <Link href="/spain">
+            <Image
+              src="./Spanish Site.svg"
+              alt=""
+              width={22}
+              height={22}
+              className="flag"
+            />
+          </Link>
+        </div>
+        <div className="header-cta">
+          <a href="">Request a callback</a>
+        </div>
       </div>
     </header>
   );
